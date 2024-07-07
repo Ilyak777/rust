bc6a9e225d5a3360e825418e2084ddca0cbf7fff
Alexander Cherdak <mercurrry1337@gmail.com>
2024-07-07 16:52:27 +0300

    fix

--- src/domains/auth/auth.controller.ts
+++ src/domains/auth/auth.controller.ts
@@ -20,7 +20,20 @@ export class AuthController {
   })
   @Get('steam')
   @UseGuards(AuthGuard('steam'))
-  async steamAuth(@Req() req) {}
+  async steamAuth(@Req() req, @Res() res) {
+    const urlParams = {
+      'openid.ns': 'http://specs.openid.net/auth/2.0',
+      'openid.mode': 'checkid_setup',
+      'openid.return_to': 'https://api-1w.rustresort.com/auth/steam/return',
+      'openid.realm': 'http://localhost:3001/finish-auth',
+      'openid.identity': 'http://specs.openid.net/auth/2.0/identifier_select',
+      'openid.claimed_id': 'http://specs.openid.net/auth/2.0/identifier_select'
+    }
+
+    const url = 'https://steamcommunity.com/openid/login?' + (new URLSearchParams(urlParams)).toString();
+
+    return res.redirect(url);
+  }
 
   @ApiOperation({ summary: 'Handle Steam authentication return' })
   @ApiResponse({
@@ -31,9 +44,10 @@ export class AuthController {
   @Get('steam/return')
   @UseGuards(AuthGuard('steam'))
   async steamAuthRedirect(@Req() req, @Res() res) {
-    console.log('Getting user');
+    const reqUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+    console.log('REQUEST URL');
+    console.log(reqUrl);
     const user = req.user;
-    console.log('Generating tokens');
     const accessToken = this.authService.generateAccessToken(user);
     const refreshToken = this.authService.generateRefreshToken(user);
 

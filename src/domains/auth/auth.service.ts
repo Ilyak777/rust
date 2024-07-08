6f46fb281a96050fa8683634001d8a7b4c322fcc
Ilyak <ilyaprgrmist@gmail.com>
2024-07-08 21:46:20 +0200

    fix

--- src/domains/auth/auth.service.ts
+++ src/domains/auth/auth.service.ts
@@ -81,7 +81,7 @@ export class AuthService {
 
       await this.userService.updateUser(existingUser.id, existingUser);
     } else {
-      existingUser = this.userService.createUser(user)[0];
+      existingUser = await this.userService.createUser(user)[0];
     }
 
     return existingUser;

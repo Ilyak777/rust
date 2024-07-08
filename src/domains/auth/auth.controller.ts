1bad0724ab3096d268b37a5b9ec440b56bcf15aa
Alexander Cherdak <mercurrry1337@gmail.com>
2024-07-08 05:43:37 +0300

    fix

--- src/domains/auth/auth.controller.ts
+++ src/domains/auth/auth.controller.ts
@@ -37,7 +37,6 @@ export class AuthController {
     const refreshToken = this.authService.generateRefreshToken(user);
     const url = `http://localhost:3030/finish-auth?access_token=${accessToken}&refresh_token=${refreshToken}`;
 
-    res.setHeader('Access-Control-Allow-Origin', 'https://rustresort.com/');
-    return res.redirect(url);
+    return { url };
   }
 }

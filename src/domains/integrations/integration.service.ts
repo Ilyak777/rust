ce06e8f88451808b61322bf42f4229cd5a549844
Ilyak <ilyaprgrmist@gmail.com>
2024-07-18 17:30:58 +0200

    deleted logs

--- src/domains/integrations/integration.service.ts
+++ src/domains/integrations/integration.service.ts
@@ -46,9 +46,6 @@ export class IntegrationService {
     const serversToGive = servers.filter((el) => {
       return el.id !== 5;
     });
-    console.log('[LOGGED]: Servers to give');
-    console.log(serversToGive);
-    console.log(skinboxCommandToSave, vipCommandToSave, userId, user.steamId);
     await serversToGive.map((el) => {
       this.serverService.executeStraightCommand(
         skinboxCommandToSave,
